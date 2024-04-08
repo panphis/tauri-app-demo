ab1f66d7814243c9b0fc83eb5e3960a27a30cb52
孤舟江海寄余生 <319874759@qq.com>
2024-04-08 22:06:38 +0800

    feat(cpu info): 添加 cpu 颜色信息

--- src/components/Chart/CpuCoreUsage.tsx
+++ src/components/Chart/CpuCoreUsage.tsx
@@ -1,42 +1,79 @@
 import { CpuCoreUsageRecorders, CpuCoresNames } from "@/interface";
-import React, { Fragment, type FC } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { Fragment, type FC, useMemo, useState } from "react";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+	Brush,
+} from "recharts";
+import { generateUniformColors } from "@/utils";
 
 type CpuCoreUsageProps = {
-	data: CpuCoreUsageRecorders,
-	cpuCoresNames: CpuCoresNames
+	data: CpuCoreUsageRecorders;
+	cpuCoresNames: CpuCoresNames;
 };
 
-export const CpuCoreUsage: FC<CpuCoreUsageProps> = ({ data, cpuCoresNames = [] }) => {
-	console.log(cpuCoresNames, data);
-	return <Fragment>
-		<div className="h-64">
+export const CpuCoreUsage: FC<CpuCoreUsageProps> = ({
+	data,
+	cpuCoresNames = [],
+}) => {
+	const colors = useMemo(() => {
+		const color = generateUniformColors(cpuCoresNames.length);
+		console.log(color);
+		return color;
+	}, [cpuCoresNames]);
 
-			<ResponsiveContainer width="100%" height="100%">
-				<LineChart
-					width={500}
-					height={300}
-					data={data}
-					margin={{
-						top: 5,
-						right: 30,
-						left: 20,
-						bottom: 5,
-					}}
-				>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="time" />
-					<YAxis />
-					<Tooltip />
-					<Legend />
-					{
-						cpuCoresNames.map((name, index) => {
-							return <Line key={name} type="monotone" isAnimationActive={false} dataKey={name} activeDot={{ r: 8 }} />
-						})
-					}
-				</LineChart>
-			</ResponsiveContainer>
-		</div>
-	</Fragment>;
+	const [brush, setBursh] = useState([0, 0]);
+
+	return (
+		<Fragment>
+			<div className='h-64'>
+				<ResponsiveContainer width='100%' height='100%'>
+					<LineChart
+						width={500}
+						height={300}
+						data={data}
+						margin={{
+							top: 5,
+							right: 30,
+							left: 20,
+							bottom: 5,
+						}}
+					>
+						<CartesianGrid strokeDasharray='3 3' />
+						<XAxis dataKey='time' />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						{cpuCoresNames.map((name, index) => {
+							return (
+								<Line
+									key={name}
+									type='monotone'
+									isAnimationActive={false}
+									dataKey={name}
+									stroke={colors[index]}
+									activeDot={{ r: 8 }}
+								/>
+							);
+						})}
+						{/* <Brush
+							dataKey='name'
+							height={30}
+							stroke='#8884d8'
+							startIndex={brush[0]}
+							endIndex={brush[1]}
+							onChange={(e) => setBursh([e?.startIndex || 0, e?.endIndex || 0])}
+						/> */}
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
+		</Fragment>
+	);
 };
-export default CpuCoreUsage
+export default CpuCoreUsage;
